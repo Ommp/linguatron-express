de1f36662d3dd0c85e2b55cd003823b782ad5354
Ommp <dallande@protonmail.com>
2024-04-28 19:00:05 +0200

    add more fields to cards table and return correct json format

--- index.ts
+++ index.ts
@@ -1,12 +1,16 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, Request, Response, response } from "express";
 // import path from "path";
 
 import bodyParser from "body-parser";
 
 import { Database } from "bun:sqlite";
 
+import cors from 'cors';
+
+
 const app: Express = express();
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -26,7 +30,7 @@ const db = new Database("mydb.sqlite");
 db.exec("PRAGMA journal_mode = WAL;");
 
 function createCardsTable() {
-  return db.query("CREATE TABLE IF NOT EXISTS cards (card_id INTEGER PRIMARY KEY, deck_id INTEGER, question TEXT, answer TEXT, FOREIGN KEY (deck_id) REFERENCES decks(deck_id) ON DELETE CASCADE)");
+  return db.query("CREATE TABLE IF NOT EXISTS cards (card_id INTEGER PRIMARY KEY, deck_id INTEGER, correct INTEGER, incorrect INTEGER, card_created TEXT, last_review_date TEXT, stage TEXT, lapses INTEGER, ease REAL, review_due_date TEXT, question TEXT, answer TEXT, FOREIGN KEY (deck_id) REFERENCES decks(deck_id) ON DELETE CASCADE)");
 }
 
 function createDecksTable() {
@@ -51,23 +55,29 @@ const deleteDeck = db.query("DELETE FROM decks WHERE deck_id = ?");
 //API
 app.get('/api/decks', (req, res) => {
   let decks = <Array<Deck>>selectAllDecks.all();
-  res.json({ decks: decks });
+  console.log(decks);
+  res.json(decks);
 });
 app.get('/api/deck/:deck_id', async (req, res) => {
   const deck = selectDeckByID.get(Number(req.params.deck_id));
-  res.json({ deck: deck });
+  res.json(deck);
 });
 app.get('/api/deck/:deck_id/cards', async (req, res) => {
   const deck = <Deck>selectDeckByID.get(Number(req.params.deck_id));
 
   const cards = <Array<Card>>selectAllCardsByDeckID.all(deck.deck_id);
 
-  res.json({ cards: cards });
+  res.json(cards);
 });
 app.post('/api/deck/create', async (req, res) => {
   try {
-    insertDeck.run({ $deckname: req.body.deckname });
-    res.send(`{"status": "success"}`);
+    if (req.body.deckname != null) {
+      insertDeck.run({ $deckname: req.body.deckname });
+      res.send(`{"status": "success"}`); 
+    } else {
+      res.send("missing deck name");
+    }
+
   } catch (error) {
     console.error(error);
     res.json(error);
@@ -87,12 +97,20 @@ app.post('/api/deck/:deck_id/card/create', async (req, res) => {
   const body = req.body;
 
   try {
-    insertCard.run({
-      $deck_id: body.deck_id,
-      $question: body.question,
-      $answer: body.answer
-    });
-    res.status(200);
+
+    if (body.deck_id != null && body.question != null && body.answer != null) {
+      insertCard.run({
+        $deck_id: body.deck_id,
+        $question: body.question,
+        $answer: body.answer
+      });
+      res.status(200);
+      res.send("success");
+    }
+    else {
+      res.send("Missing required data");
+    }
+
   } catch (error) {
     console.error(error);
     res.send(error);
